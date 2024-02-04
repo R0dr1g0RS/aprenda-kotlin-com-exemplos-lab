d05567b33fd00ee622f01a6e2966491b51f4d9de
Rodrigo Rodrigues Santos <116824752+R0dr1g0RS@users.noreply.github.com>
2024-02-04 19:37:58 -0300

    Desafio Concluído!

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,75 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, val ID: Int)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+    
+    fun listarConteudos(){
+        print("|")
+        for (c in conteudos) {
+            print(" ${c.nome} |")
+        }
+        println("")
+    }
+  
+    fun removerMatricula(userName: String){
+        var indice = 0
+        var encontrou = false
+        for (user in inscritos){
+            if (user.nome == userName) encontrou = true else{}
+            if (!encontrou) indice++ else {}
+        }
+        
+        inscritos.removeAt(indice)
+    }
+    
+    fun removerMatricula(userID: Int){
+        var indice = 0
+        var encontrou = false
+        for (user in inscritos){
+            if (user.ID == userID) encontrou = true else {}
+            if (!encontrou) indice++ else {}
+        }
+        
+        inscritos.removeAt(indice)
+    }
+
+    fun buscarInscrito(userName: String){
+        for (user in inscritos){
+            if (user.nome == userName) println("Usuário: ${user.nome} está inscrito nessa formação!") else {}
+        }
+    }
+    fun buscarInscrito(userID: Int){
+        for (user in inscritos){
+            if (user.ID == userID) println("Usuário: ${user.nome} está inscrito nessa formação!") else {}
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val user1 = Usuario("Rodrigo", 1)
+    val user2 = Usuario("Joao", 2)
+    val user3 = Usuario("Alexia", 3)
+    val conteudos = listOf<ConteudoEducacional>(ConteudoEducacional("Versionamento de Codigo", 120), 
+                                                ConteudoEducacional("POO", 60), 
+                                                ConteudoEducacional("Funções", 60)
+                                               )
+    val formacaoKotlin = Formacao("Kotlin", conteudos, Nivel.BASICO)
+    formacaoKotlin.matricular(user1)
+    formacaoKotlin.matricular(user2)
+    formacaoKotlin.matricular(user3)
+    
+    formacaoKotlin.buscarInscrito("Joao")
+    formacaoKotlin.listarConteudos()
+    
 }
